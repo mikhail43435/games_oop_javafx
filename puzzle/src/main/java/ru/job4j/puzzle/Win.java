79fb022ed39fde35b35f1c86a62bfeeb8912be38
mikhail43435 <hommp2009@mail.ru>
2020-07-21 17:21:21 +0300

    Сокобан. Тестовое задание. [#325094]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,7 +2,54 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        boolean rsl = true;
+        boolean rsl = hasFiveInRowOrColumn(board);
         return rsl;
     }
+
+    // проверка одинаковых значений по вертикали
+    public static boolean monoVertical(int[][] board, int column) {
+        boolean result = true;
+        for (int index = 0; index < board.length; index++) {
+            if (board[index][column] != 1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    // проверка одинаковых значений по горизонтали
+    public static boolean monoHorizontal(int[][] board, int row) {
+        boolean result = true;
+
+        for (int index = 0; index < board[row].length; index++) {
+            if (board[row][index] != 1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static boolean hasFiveInRowOrColumn(int[][] board) {
+        boolean result = false;
+        int lengthOfMatrix = board.length;
+        int neededSymbol = 1;
+        //boolean winIsFoound;
+
+        for (int index = 0; index < lengthOfMatrix; index++) {
+            if (board[index][index] == neededSymbol) { // нашли Х в ячейке
+                // ищем по вертикали и горизонтали
+                if (monoHorizontal(board, index) || monoVertical(board, index)) {
+                    return true;
+                }
+            }
+        }
+        return result;
+    }
+
 }
+
+
+
+
